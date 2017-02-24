5b0fe84b187f059eb02ffef4bc00ed1687d6c122
Ana Schintee <as42g15@soton.ac.uk>
2017-02-24 16:33:44 +0000

    Added parser for csv files

---
+++ AdAuctionDashboard/src/csvClickReader.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by ANA MARIA on 23/02/2017.
+ */
+public class csvClickReader {
+    public static void main(String[] args) {
+        String csvFile = "C:\\Users\\ANA MARIA\\Desktop\\Dashboard\\2_week_campaign_2\\click_log.csv";
+        BufferedReader br = null;
+        String line = "";
+        String splitBy = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                String[] impression = line.split(splitBy);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ AdAuctionDashboard/src/csvServerReader.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by ANA MARIA on 23/02/2017.
+ */
+public class csvServerReader {
+    public static void main(String[] args) {
+        String csvFile = "C:\\Users\\ANA MARIA\\Desktop\\Dashboard\\2_week_campaign_2\\server_log.csv";
+        BufferedReader br = null;
+        String line = "";
+        String splitBy = ",";
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                String[] impression = line.split(splitBy);
+                System.out.println(impression[0]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
